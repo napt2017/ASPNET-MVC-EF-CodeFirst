a30e620f0de5dcae9b0d13b0e2e7b96cebc83f6f
ngan <NgocAnh.Nguyen@episerver.com>
2020-04-10 17:03:13 +0700

    Add ajax call via native browser support and using JQuery

---
+++ Controllers/AjaxController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace ASPNET_MVC_EF_CodeFirst.Controllers
+{
+    public class AjaxController : Controller
+    {
+        // GET: Ajax
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
